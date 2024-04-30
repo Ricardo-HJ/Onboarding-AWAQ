df9856a4db8edeb0cdfed4959a063449659d475f
Adrian-TJ <atrevino136@gmail.com>
2024-04-30 13:45:15 -0600

    Creacion de la clase usuario y nuget de MySQL

---
+++ Onboarding AWAQ/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace Onboarding_AWAQ
+{
+	public class Usuario
+	{
+		public int Id { get; set; }
+		public string Correo { get; set; }
+		public string Nombre { get; set; }
+		public string Contraseña { get; set; }
+	}
+}
