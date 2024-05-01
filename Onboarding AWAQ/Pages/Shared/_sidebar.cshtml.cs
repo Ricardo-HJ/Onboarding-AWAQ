f84dc64ec60cf7ae9bc58794f136310b1828f143
Ricardo-HJ <a00837337@tec.mx>
2024-05-01 14:34:01 -0600

    Update dashboard, leaderboard, and registro pages, created sidebar partial and layout

---
+++ Onboarding AWAQ/Pages/Shared/_sidebar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Onboarding_AWAQ.Pages.Shared
+{
+    public class _sidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
