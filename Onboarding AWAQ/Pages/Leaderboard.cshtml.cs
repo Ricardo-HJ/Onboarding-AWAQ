54b3f8da47956e5d339e70af7cbfc6755dc76c98
Ricardo-HJ <a00837337@tec.mx>
2024-04-30 23:50:01 -0600

    Sidebar finished and other details

---
+++ Onboarding AWAQ/Pages/Leaderboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Onboarding_AWAQ.Pages
+{
+    public class LeaderboardcshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
