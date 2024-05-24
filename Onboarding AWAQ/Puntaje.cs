0edf26e1cdf3fd11dd97d3258969f2ca9dd4dc72
Adrian-TJ <atrevino136@gmail.com>
2024-05-24 14:26:01 -0600

    Cambios en la logica del leaderboard para incorporar C#

---
+++ Onboarding AWAQ/Puntaje.cs
@@ -0,0 +1,13 @@
+﻿namespace Onboarding_AWAQ
+{
+    public class Puntaje
+    {
+        public string src { get; set; }
+        public int position { get; set; }
+        public string name { get; set; }
+        public int puntaje { get; set; }
+        public int tiempoJugado { get; set; }
+        public string departamento { get; set; }
+        public string terminado { get; set; }
+    }
+}
