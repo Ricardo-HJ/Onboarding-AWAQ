975f9a7258a3dfb33e7a089b4e5db5407f182eed
Adrian-TJ <atrevino136@gmail.com>
2024-05-24 19:05:03 -0600

    Leaderboard logic update

--- Onboarding AWAQ/Pages/Leaderboard.cshtml.cs
+++ Onboarding AWAQ/Pages/Leaderboard.cshtml.cs
@@ -14,6 +14,33 @@ namespace Onboarding_AWAQ.Pages
         public bool admin { get; set; }
         public string src { get; set; }
         public List<Puntaje> Leaderboard { get; set; }
+        static async Task<List<Puntaje>> PuntajeUsuario()
+        {
+            HttpClient cliente = new HttpClient();
+            cliente.BaseAddress = new Uri("https://localhost:7117/");
+            cliente.DefaultRequestHeaders.Accept.Clear();
+            cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            List<Puntaje> Leaderboard = new List<Puntaje> { };
+            try
+            {
+                //Path interno del end point
+                HttpResponseMessage Res = await cliente.GetAsync("api/Web/getLeaderboard");
+                //Checar si el estatus es correcto del HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Obtener el response recibido web api
+                    var apiResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializing la respuesta del web api y guardarlo en la lista
+                    Leaderboard = JsonConvert.DeserializeObject<List<Puntaje>>(apiResponse);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.InnerException);
+                Console.WriteLine(e.Message);
+            }
+            return Leaderboard;
+        }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -26,35 +53,6 @@ namespace Onboarding_AWAQ.Pages
 
             Leaderboard = await PuntajeUsuario();
             return Page();
-        }
-
-        static async Task<List<Puntaje>> PuntajeUsuario()
-        {
-            HttpClient cliente = new HttpClient();
-            cliente.BaseAddress = new Uri("https://localhost:7117/");
-            cliente.DefaultRequestHeaders.Accept.Clear();
-            cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            List<Puntaje> Leaderboard = new List<Puntaje>();
-            try
-            {
-                //Path interno del end point
-                HttpResponseMessage Res = await cliente.GetAsync("getLeaderboard");
-                //Checar si el estatus es correcto del HttpClient
-                if (Res.IsSuccessStatusCode)
-                {
-                    //Obtener el response recibido web api
-                    var apiResponse = Res.Content.ReadAsStringAsync().Result;
-                    //Deserializing la respuesta del web api y guardarlo en la lista
-                    Leaderboard = JsonConvert.DeserializeObject<List<Puntaje>>(apiResponse);
-                    Console.WriteLine(Leaderboard);
-                }
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine(e.InnerException);
-                Console.WriteLine(e.Message);
-            }
-            return Leaderboard;
         }
     }
 }
